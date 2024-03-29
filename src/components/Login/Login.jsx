167f7a52b3e213a6a7e8aa3db480ced0d06b2687
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2024-03-29 19:04:34 -0300

    useform agregado al formulario, y el estilo de mobile y tablet fue corregido

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -18,26 +18,48 @@ const Login = () => {
         <img src={backgroundImg} className="img-blur-ingreso" />
       </div>
 
-      <div className="form-Img-Container container bg-light p-3 rounded-3">
+      <div className="form-Img-Container container-fluid bg-light p-3 rounded-3">
+        <h2 className="text-center mb-2">¡Bienvenido!</h2>
+
         <div className="row">
-          <div className="img-form col-lg-6 bg-dark">
+          <div className="img-form col-lg-6 bg-dark d-none d-lg-block">
             <img src="" />
           </div>
 
           <div className="col-lg-6">
-            <Form
-              className="d-flex flex-column gap-3"
-              onSubmit={handleSubmit(validarFormulario)}
-            >
+            <Form onSubmit={handleSubmit(validarFormulario)}>
               <Form.Group>
                 <Form.Label htmlFor="correo">Correo</Form.Label>
                 <Form.Control
                   required
                   type="text"
                   placeholder="ejemplo@correo.com"
+                  className="mb-3"
                   id="correo"
+                  {...register("email", {
+                    required:
+                      "El Email es Obligatorio. Ingrese un Email válido, por favor!",
+                    minLength: {
+                      value: 4,
+                      message: "El Email debe contener al menos 4 caracteres",
+                    },
+                    maxLength: {
+                      value: 250,
+                      message:
+                        "El email debe contener como máximo 250 caracteres",
+                    },
+                    pattern: {
+                      value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
+                      message:
+                        "Ingrese una dirección de correo electrónico válida",
+                    },
+                  })}
                 />
               </Form.Group>
+
+              <Form.Text className="text-danger fw-bold">
+                {errors.email?.message}
+              </Form.Text>
 
               <Form.Group>
                 <Form.Label htmlFor="password">Contraseña</Form.Label>
@@ -45,11 +67,26 @@ const Login = () => {
                   required
                   type="password"
                   placeholder="contraseña"
+                  className="mb-4"
                   id="password"
+                  {...register("password", {
+                    required:
+                      "El Password es Obligatorio. Ingrese un Password, por favor!",
+                    minLength: {
+                      value: 4,
+                      message:
+                        "El Password debe contener al menos 4 caracteres.",
+                    },
+                  })}
                 />
               </Form.Group>
+              <Form.Text className="text-danger fw-bold">
+                {errors.password?.message}
+              </Form.Text>
 
-              <Button>Ingresar</Button>
+              <div className="text-center">
+                <Button>Ingresar</Button>
+              </div>
             </Form>
           </div>
         </div>
