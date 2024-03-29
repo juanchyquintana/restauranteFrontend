58ec523e4f0f20ddd02ce8df9d3ec314a63c565d
Nano <luxorius07@gmail.com>
2024-03-29 01:20:07 -0300

    se crearon los componentes de la pagina principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Menu from './components/common/Menu'
+import Inicio from './components/pages/Inicio'
+import Administrador from './components/pages/Administrador'
+import Error404 from './components/pages/Error404'
+import Footer from './components/common/Footer'
 
 function App() {
 
   return (
     <>
-        <h1>Hola Mundo</h1>
+        <Menu></Menu>
+        <Inicio></Inicio>
+        <Administrador></Administrador>
+        <Error404></Error404>
+        <Footer></Footer>
     </>
   )
 }
