a3782b4b3323b9f1df32f682f1ebc663f6ce0d9f
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2024-03-29 18:33:29 -0300

    formulario con boton centrado en dispositos mobile, y tablets

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,13 +1,61 @@
-import backgroundImg from '../../assets/Login/backgroundImg.webp'
-import './login.css'
-
+import { Button, Form } from "react-bootstrap";
+import backgroundImg from "../../assets/Login/backgroundImg.webp";
+import "./login.css";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-  return (
-    <section className='login-container container-fluid p-0 m-0 bg-black'>
-        <img src={backgroundImg} className='img-ingreso'/>
-    </section>
-  )
-}
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
 
-export default Login
+  const validarFormulario = () => {};
+
+  return (
+    <section className="login-container p-0 m-0 mainPage d-flex flex-column justify-content-center">
+      <div className="background-img-container bg-black">
+        <img src={backgroundImg} className="img-blur-ingreso" />
+      </div>
+
+      <div className="form-Img-Container container bg-light p-3 rounded-3">
+        <div className="row">
+          <div className="img-form col-lg-6 bg-dark">
+            <img src="" />
+          </div>
+
+          <div className="col-lg-6">
+            <Form
+              className="d-flex flex-column gap-3"
+              onSubmit={handleSubmit(validarFormulario)}
+            >
+              <Form.Group>
+                <Form.Label htmlFor="correo">Correo</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder="ejemplo@correo.com"
+                  id="correo"
+                />
+              </Form.Group>
+
+              <Form.Group>
+                <Form.Label htmlFor="password">Contraseña</Form.Label>
+                <Form.Control
+                  required
+                  type="password"
+                  placeholder="contraseña"
+                  id="password"
+                />
+              </Form.Group>
+
+              <Button>Ingresar</Button>
+            </Form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
