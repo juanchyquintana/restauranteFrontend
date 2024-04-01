084e88593eac2a5688b5d59d17b22cedf985e4c9
K Stephan Torres <kevinstephantorres@gmail.com>
2024-04-01 19:06:12 -0300

    Se agrego los elemntos para la pagina error 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Registrarse from "./components/Registrarse";
 import Login from "./components/Login/Login";
+import Error404 from "./components/pages/error404/Error404";
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
         <Routes>
           <Route exact path="/registrarse" element={<Registrarse />}></Route>
           <Route exact path="/ingresar" element={<Login />} />
+
+          <Route path="*" element={<Error404></Error404>}></Route>
         </Routes>
       </BrowserRouter>
     </>
