3f6a523aac9acd86360c0415d062c92b4473d8ee
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2024-04-01 17:03:06 -0300

    envolvi a las rutas con el header y el footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,21 @@ import Administrador from "./components/pages/Administrador";
 import Registrarse from "./components/Registrarse";
 import Login from "./components/Login/Login";
 import Inicio from "./components/pages/Inicio";
+import Menu from "./components/common/Menu";
+import Footer from "./components/common/Footer";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+      <Menu />
         <Routes>
           <Route exact path="/" element={<Inicio />}/>
           <Route exact path="/registrarse" element={<Registrarse />}/>
           <Route exact path="/ingresar" element={<Login />} />
           <Route exact path="/administrador" element={<Administrador />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );
