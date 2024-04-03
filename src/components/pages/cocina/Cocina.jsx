5dc22bc1dc3ae8cb6af56719750944ce6ee0dc61
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2024-04-03 15:06:21 -0300

    agregue como prueba 2 cards dentro de cocina, para que quede maquetado el ejemplo

--- src/components/pages/cocina/Cocina.jsx
+++ src/components/pages/cocina/Cocina.jsx
@@ -4,26 +4,47 @@ import CardProductoCocina from "./CardProdCocina";
 
 const Cocina = () => {
   return (
-    <section className="mainPage bg-dark">
-      <Container className="cocina-container overflow-hidden">
-        <h2 className="text-white mb-4">Pedidos pendientes: 2</h2>
+    <section className="mainPage bg-light">
+      <Container className="cocina-container overflow-hidden mb-5">
+        <h2 className="mb-4">Pedidos pendientes: 2</h2>
         <Container fluid>
-          <Card>
+          <Card className="mb-3">
             <Card.Body className="card-padre-body">
               <Card.Title className="mb-3">Pedido 23</Card.Title>
               {/* <Card.Subtitle className="mb-2 text-muted">
                 5:03
               </Card.Subtitle> */}
-              <Container fluid className="d-flex p-0">
+              <Container fluid className="d-flex p-0 gap-4">
                 <CardProductoCocina />
                 <CardProductoCocina />
               </Container>
+              
             </Card.Body>
             <Card.Footer className="d-flex justify-content-between align-items-center">
                 <p className="m-0 btn btn-success">5:03</p>
                 <Button>Finalizar</Button>
             </Card.Footer>
           </Card>
+          
+          <Card className="mb-3">
+            <Card.Body className="card-padre-body">
+              <Card.Title className="mb-3">Pedido 23</Card.Title>
+              {/* <Card.Subtitle className="mb-2 text-muted">
+                5:03
+              </Card.Subtitle> */}
+              <Container fluid className="d-flex p-0 gap-4">
+                <CardProductoCocina />
+                <CardProductoCocina />
+              </Container>
+              
+            </Card.Body>
+            <Card.Footer className="d-flex justify-content-between align-items-center">
+                <p className="m-0 btn btn-success">5:03</p>
+                <Button>Finalizar</Button>
+            </Card.Footer>
+          </Card>
+
+          
         </Container>
       </Container>
     </section>
