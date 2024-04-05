1b5451f7e468b953d03a47e6946e80fe609def17
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-04-05 16:46:20 -0300

    se crearon el carrousel de las reseñas de los clientes

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Menu from "./components/common/Menu";
 import Footer from "./components/common/Footer";
 import Error404 from "./components/pages/error404/Error404";
 import Cocina from "./components/pages/cocina/Cocina";
+import Reviews from "./components/Reseñas/Reviews";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route exact path="/ingresar" element={<Login />} />
           <Route exact path="/administrador" element={<Administrador />} />
           <Route exact path="/cocina" element={<Cocina />}/>
+          <Route exact path="/reseñas" element={<Reviews />}/>
           <Route path="*" element={<Error404></Error404>}></Route>
         </Routes>
         <Footer />
