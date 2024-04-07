2642b22dae3da110dd961681d0bf94b498090b39
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2024-04-07 11:58:48 -0300

    correccion al footer, cambiando a etiqueta main desde app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css'
+import "./App.css";
 import Administrador from "./components/pages/Administrador";
 import Registrarse from "./components/Registrarse";
 import Login from "./components/Login/Login";
@@ -13,24 +13,25 @@ import Reviews from "./components/Reseñas/Reviews";
 import Nosotros from "./components/pages/nosotros/Nosotros";
 import Carrito from "./components/pages/carrito/Carrito";
 
-
 function App() {
   return (
     <>
       <BrowserRouter>
-      <Menu />
-        <Routes>
-          <Route exact path="/" element={<Inicio />}/>
-          <Route exact path="/registrarse" element={<Registrarse />}/>
-          <Route exact path="/ingresar" element={<Login />} />
-          <Route exact path="/administrador" element={<Administrador />} />
-          <Route exact path="/cocina" element={<Cocina />}/>
-          <Route exact path="/carrito" element={<Carrito />}/>
-          <Route exact path="/contacto" element={<Contacto />}/>
-          <Route exact path="/reseñas" element={<Reviews />}/>
-          <Route exact path="/nosotros" element={<Nosotros />}/>
-          <Route path="*" element={<Error404></Error404>}></Route>
-        </Routes>
+        <Menu />
+        <main>
+          <Routes>
+            <Route exact path="/" element={<Inicio />} />
+            <Route exact path="/registrarse" element={<Registrarse />} />
+            <Route exact path="/ingresar" element={<Login />} />
+            <Route exact path="/administrador" element={<Administrador />} />
+            <Route exact path="/cocina" element={<Cocina />} />
+            <Route exact path="/carrito" element={<Carrito />} />
+            <Route exact path="/contacto" element={<Contacto />} />
+            <Route exact path="/reseñas" element={<Reviews />} />
+            <Route exact path="/nosotros" element={<Nosotros />} />
+            <Route path="*" element={<Error404></Error404>}></Route>
+          </Routes>
+        </main>
         <Footer />
       </BrowserRouter>
     </>
