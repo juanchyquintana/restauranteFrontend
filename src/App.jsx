e3930d33234936048cdea47d0bcf15bef4acd335
Nano <luxorius07@gmail.com>
2024-04-07 13:50:28 -0300

    se realizo pagina de menu sin logica solo maquetado hardcodeado

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,24 @@ import Menu from "./components/common/Menu";
 import Footer from "./components/common/Footer";
 import Error404 from "./components/pages/error404/Error404";
 import Cocina from "./components/pages/cocina/Cocina";
+import SeccionMenu from "./components/pages/menus/SeccionMenu";
+
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Menu />
+      {/* <Menu /> 
         <Routes>
-          <Route exact path="/" element={<Inicio />} />
-          <Route exact path="/registrarse" element={<Registrarse />} />
+          <Route exact path="/" element={<Inicio />}/>
+          <Route exact path="/registrarse" element={<Registrarse />}/>
           <Route exact path="/ingresar" element={<Login />} />
           <Route exact path="/administrador" element={<Administrador />} />
-          <Route exact path="/cocina" element={<Cocina />} />
+          <Route exact path="/cocina" element={<Cocina />}/>
+          <Route path="*" element={<Error404></Error404>}></Route>       
         </Routes>
-        <Footer />
+        <Footer />  */}
+        <SeccionMenu></SeccionMenu>
       </BrowserRouter>
     </>
   );
---
+++ src/components/pages/menus/SeccionMenu.jsx
@@ -0,0 +1,195 @@
+import { Container } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import "./seccionMenu.css";
+import bannerComida from './bannerImg/comidaChina.jpg'
+import bannerBebida from './bannerImg/bebidaChina.jpg'
+
+const SeccionMenu = () => {
+  return (
+    <section>
+      <img
+        className="banner"
+        src={bannerComida}
+        alt="plato con postre y un libro de receta"
+      />
+      <Container className="my-4  pb-4">
+        <div className="row ">
+          <div className="col-md-4 col-lg-3 mb-3 ">
+            <Card>
+              <Card.Img
+                variant="top"
+                src="https://cadenaser.com/resizer/xRCT4u61WV8B6FPDIZ9sVEsJP1M=/736x552/filters:format(jpg):quality(70)/cloudfront-eu-central-1.images.arcpublishing.com/prisaradio/6YDRXOMYH5LOLD7S6G2YINMS74.jpg"
+              />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+            </div>
+          <div className="col-md-4 col-lg-3 mb-3 ">
+            <Card>
+              <Card.Img
+                variant="top"
+                src="https://cadenaser.com/resizer/xRCT4u61WV8B6FPDIZ9sVEsJP1M=/736x552/filters:format(jpg):quality(70)/cloudfront-eu-central-1.images.arcpublishing.com/prisaradio/6YDRXOMYH5LOLD7S6G2YINMS74.jpg"
+              />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+            </div>
+          <div className="col-md-4 col-lg-3 mb-3 ">
+            <Card>
+              <Card.Img
+                variant="top"
+                src="https://cadenaser.com/resizer/xRCT4u61WV8B6FPDIZ9sVEsJP1M=/736x552/filters:format(jpg):quality(70)/cloudfront-eu-central-1.images.arcpublishing.com/prisaradio/6YDRXOMYH5LOLD7S6G2YINMS74.jpg"
+              />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+            </div>
+          <div className="col-md-4 col-lg-3 mb-3 ">
+            <Card>
+              <Card.Img
+                variant="top"
+                src="https://cadenaser.com/resizer/xRCT4u61WV8B6FPDIZ9sVEsJP1M=/736x552/filters:format(jpg):quality(70)/cloudfront-eu-central-1.images.arcpublishing.com/prisaradio/6YDRXOMYH5LOLD7S6G2YINMS74.jpg"
+                className="img-fluid"
+              />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+            </div>
+          <div className="col-md-4 col-lg-3 mb-3 ">
+            <Card>
+              <Card.Img
+                variant="top"
+                src="https://cadenaser.com/resizer/xRCT4u61WV8B6FPDIZ9sVEsJP1M=/736x552/filters:format(jpg):quality(70)/cloudfront-eu-central-1.images.arcpublishing.com/prisaradio/6YDRXOMYH5LOLD7S6G2YINMS74.jpg"
+              />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+            </div>
+        </div>
+      </Container>
+
+      <img
+        className="banner mt-5"
+        src={bannerBebida}
+      />
+      <Container className="my-4 ">
+        <div className="row ">
+          <div className="col-md-4 col-lg-3 mb-3 ">
+            <Card>
+              <Card.Img
+                variant="top"
+                src="https://www.distribuidora7y3.com.uy/wp-content/uploads/2024/02/P482477-2.webp"
+              />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+            </div>
+          <div className="col-md-4 col-lg-3 mb-3 ">
+            <Card>
+              <Card.Img
+                variant="top"
+                src="https://www.distribuidora7y3.com.uy/wp-content/uploads/2024/02/P482477-2.webp"
+              />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+            </div>
+          <div className="col-md-4 col-lg-3 mb-3 ">
+            <Card>
+              <Card.Img
+                variant="top"
+                src="https://www.distribuidora7y3.com.uy/wp-content/uploads/2024/02/P482477-2.webp"
+              />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+            </div>
+          <div className="col-md-4 col-lg-3 mb-3 ">
+            <Card>
+              <Card.Img
+                variant="top"
+                src="https://www.distribuidora7y3.com.uy/wp-content/uploads/2024/02/P482477-2.webp"
+                className="img-fluid"
+              />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+            </div>
+          <div className="col-md-4 col-lg-3 mb-3 ">
+            <Card>
+              <Card.Img
+                variant="top"
+                src="https://www.distribuidora7y3.com.uy/wp-content/uploads/2024/02/P482477-2.webp"
+              />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+            </div>
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+export default SeccionMenu;
