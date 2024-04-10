50a0890601105141f3c1259e491322cdf86bd534
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-04-10 19:55:21 -0300

    se maquetó la tabla para los usuarios

--- src/components/pages/PanelUsuarios.jsx
+++ src/components/pages/PanelUsuarios.jsx
@@ -1,10 +1,34 @@
-import { Container } from "react-bootstrap";
+import { Container, Table } from "react-bootstrap";
 
 const PanelUsuarios = () => {
   return (
-    <Container>
-      hola
-    </Container>
+    <>
+      <Container>
+        <section className="my-5">
+          <div className="d-flex justify-content-between align-items-center mt-5">
+            <h1 className="display-5">Panel de Usuarios</h1>
+          </div>
+
+          <hr />
+
+          <Table responsive>
+            <thead>
+              <tr>
+                <th>Usuario</th>
+                <th>Email</th>
+                <th>Estado</th>
+                <th>Tipo Usuario</th>
+                <th>Opciones</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              
+            </tbody>
+          </Table>
+        </section>
+      </Container>
+    </>
   )
 }
 
