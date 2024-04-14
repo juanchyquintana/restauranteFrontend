299d124228ff45ae7b2ad440222c1e01a0d2a58d
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-04-14 01:15:16 -0300

    se creó la funcion para formatear fecha y luego se crea la funcion para cerrar la caja y que todo vuelva de 0

--- src/components/pages/PanelGanancias.jsx
+++ src/components/pages/PanelGanancias.jsx
@@ -7,10 +7,12 @@ import {
 import { useEffect, useState } from "react";
 import bambu from "../../assets/bambu-fondo.jpg";
 import "./panelganancias.css";
+import Swal from "sweetalert2";
 
 const PanelGanancias = () => {
   const [gananciasDia, setGananciasDia] = useState(0);
   const [cantidadPedidos, setCantidadPedidos] = useState(0);
+  const [fechaActual, setFechaActual] = useState("");
 
   const obtenerGanancias = async () => {
     const ganancias = await obtenerGananciasDia();
@@ -22,14 +24,44 @@ const PanelGanancias = () => {
     setCantidadPedidos(cantidad);
   };
 
+  const formatearFecha = (fecha) => {
+    const opciones = { year: "numeric", month: "long", day: "numeric" };
+    return fecha.toLocaleDateString("es-AR", opciones);
+  };
+
+  const fecha = new Date();
+
   const cerrarCajaHandler = async () => {
+    const fechaFormateada = formatearFecha(fecha);
+
+    await Swal.fire({
+      title: "Caja cerrada",
+      text: `Caja cerrada el ${fechaFormateada}.\n Ganancias del día: ${gananciasDia}\nCantidad de pedidos: ${cantidadPedidos}`,
+      icon: "success",
+      confirmButtonText: "Aceptar",
+    });
+
+    const datosCaja = { gananciasDia, cantidadPedidos, fechaCierreCaja: fecha.getTime() };
+    localStorage.setItem("datosCaja", JSON.stringify(datosCaja));
+
     await cerrarCaja();
     setGananciasDia(0);
+    setCantidadPedidos(0);
   };
 
   useEffect(() => {
     obtenerGanancias();
     pedidoCantidad();
+
+    const fechaFormateada = formatearFecha(fecha);
+    setFechaActual(fechaFormateada);
+
+    const datosCajaGuardados = localStorage.getItem("datosCaja");
+    if (datosCajaGuardados) {
+      const datosCaja = JSON.parse(datosCajaGuardados);
+      setGananciasDia(datosCaja.gananciasDia);
+      setCantidadPedidos(datosCaja.cantidadPedidos);
+    }
   }, []);
 
   return (
@@ -43,12 +75,13 @@ const PanelGanancias = () => {
           <div className="col-lg-6 d-flex flex-column justify-content-center text-center">
             <div className="d-flex flex-column justify-content-center mb-3">
               <h1 className="display-6">Ganancias del día:</h1>
+              <span className="fw-bold fs-3 text-uppercase">{fechaActual}</span>
               <span className="fw-bold fs-3">${gananciasDia}</span>
             </div>
 
             <div className="d-flex flex-column justify-content-center mb-3">
               <h1 className="display-6">Pedidos de la Fecha: </h1>
-              <span className="fw-bold fs-3">${cantidadPedidos}</span>
+              <span className="fw-bold fs-3">{cantidadPedidos}</span>
             </div>
 
             <Button
