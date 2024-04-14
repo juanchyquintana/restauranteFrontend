7b1186bc0b70b0d03e17ed5725437f77af8a7de2
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-04-13 23:51:44 -0300

    se le dió color a la página para que sea más bonita

--- src/components/pages/PanelGanancias.jsx
+++ src/components/pages/PanelGanancias.jsx
@@ -1,5 +1,12 @@
-import { obtenerGananciasDia, cerrarCaja, obtenerCantidadPedidosDia } from "../../helpers/pedidos";
+import { Button, Container, Row } from "react-bootstrap";
+import {
+  obtenerGananciasDia,
+  cerrarCaja,
+  obtenerCantidadPedidosDia,
+} from "../../helpers/pedidos";
 import { useEffect, useState } from "react";
+import bambu from "../../assets/bambu-fondo.jpg";
+import "./panelganancias.css";
 
 const PanelGanancias = () => {
   const [gananciasDia, setGananciasDia] = useState(0);
@@ -13,24 +20,48 @@ const PanelGanancias = () => {
   const pedidoCantidad = async () => {
     const cantidad = await obtenerCantidadPedidosDia();
     setCantidadPedidos(cantidad);
-};
+  };
 
   const cerrarCajaHandler = async () => {
-    await cerrarCaja()
-    setGananciasDia(0)
-  }
+    await cerrarCaja();
+    setGananciasDia(0);
+  };
 
   useEffect(() => {
     obtenerGanancias();
-    pedidoCantidad()
+    pedidoCantidad();
   }, []);
 
   return (
-    <div className="container my-5">
-      <h1>Ganancias del día: ${gananciasDia}</h1>
-      <h2>Cantidad de pedidos realizados hoy: {cantidadPedidos}</h2>
-      <button onClick={cerrarCajaHandler}>Cerrar caja</button>
-    </div>
+    <Container className="d-flex flex-column justify-content-center my-5 mainPage ganancias-containe">
+      <div className="imagen-contenedora container-fluid bg-light p-3 p-lg-0 rounded-3 border shadow">
+        <Row className="m-0">
+          <div className="img-form-div col-lg-6 d-none d-lg-flex rounded-start-3 p-0">
+            <img src={bambu} className="img-ganancias" />
+          </div>
+
+          <div className="col-lg-6 d-flex flex-column justify-content-center text-center">
+            <div className="d-flex flex-column justify-content-center mb-3">
+              <h1 className="display-6">Ganancias del día:</h1>
+              <span className="fw-bold fs-3">${gananciasDia}</span>
+            </div>
+
+            <div className="d-flex flex-column justify-content-center mb-3">
+              <h1 className="display-6">Pedidos de la Fecha: </h1>
+              <span className="fw-bold fs-3">${cantidadPedidos}</span>
+            </div>
+
+            <Button
+              variant="danger"
+              className="fw-bold text-uppercase mt-4"
+              onClick={cerrarCajaHandler}
+            >
+              Cerrar caja
+            </Button>
+          </div>
+        </Row>
+      </div>
+    </Container>
   );
 };
 
