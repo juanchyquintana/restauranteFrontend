114f429e47e1c23a164744b84467053c16ec8035
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-04-14 14:17:53 -0300

    se agregó un banner

--- src/components/pages/PanelGanancias.jsx
+++ src/components/pages/PanelGanancias.jsx
@@ -6,6 +6,7 @@ import {
 } from "../../helpers/pedidos";
 import { useEffect, useState } from "react";
 import bambu from "../../assets/bambu-fondo.jpg";
+import banner from "../../assets/chicosConversando.jpg";
 import "./panelganancias.css";
 import Swal from "sweetalert2";
 
@@ -77,37 +78,45 @@ const PanelGanancias = () => {
   }, []);
 
   return (
-    <Container className="d-flex flex-column justify-content-center my-5 mainPage ganancias-containe">
-      <div className="imagen-contenedora container-fluid bg-light p-3 p-lg-0 rounded-3 border shadow">
-        <Row className="m-0">
-          <div className="img-form-div col-lg-6 d-none d-lg-flex rounded-start-3 p-0">
-            <img src={bambu} className="img-ganancias" />
-          </div>
-
-          <div className="col-lg-6 d-flex flex-column justify-content-center text-center">
-            <div className="d-flex flex-column justify-content-center mb-3">
-              <h1 className="display-6">Ganancias del día:</h1>
-              <span className="fw-bold fs-3 text-uppercase">{fechaActual}</span>
-              <span className="fw-bold fs-3">${gananciasDia}</span>
-            </div>
-
-            <div className="d-flex flex-column justify-content-center mb-3">
-              <h1 className="display-6">Pedidos de la Fecha: </h1>
-              <span className="fw-bold fs-3">{cantidadPedidos}</span>
-            </div>
-
-            <Button
-              variant="danger"
-              className="fw-bold text-uppercase mt-4"
-              onClick={cerrarCajaHandler}
-            >
-              Cerrar caja
-            </Button>
-          </div>
-        </Row>
+    <section>
+      <div className="banner-container">
+        <img className="banner" src={banner} alt="plato con comida china" />
+        <h2 className="bannerTitulo mt-5">利润 Ganancias</h2>
       </div>
 
-      {/* <div className="d-flex flex-column justify-content-center mb-3">
+      <Container className="d-flex flex-column justify-content-center my-5 mainPage ganancias-containe">
+        <div className="imagen-contenedora container-fluid bg-light p-3 p-lg-0 rounded-3 border shadow">
+          <Row className="m-0">
+            <div className="img-form-div col-lg-6 d-none d-lg-flex rounded-start-3 p-0">
+              <img src={bambu} className="img-ganancias" />
+            </div>
+
+            <div className="col-lg-6 d-flex flex-column justify-content-center text-center">
+              <div className="d-flex flex-column justify-content-center mb-3">
+                <h1 className="display-6">Ganancias del día:</h1>
+                <span className="fw-bold fs-3 text-uppercase">
+                  {fechaActual}
+                </span>
+                <span className="fw-bold fs-3">${gananciasDia}</span>
+              </div>
+
+              <div className="d-flex flex-column justify-content-center mb-3">
+                <h1 className="display-6">Pedidos de la Fecha: </h1>
+                <span className="fw-bold fs-3">{cantidadPedidos}</span>
+              </div>
+
+              <Button
+                variant="danger"
+                className="fw-bold text-uppercase mt-4"
+                onClick={cerrarCajaHandler}
+              >
+                Cerrar caja
+              </Button>
+            </div>
+          </Row>
+        </div>
+
+        {/* <div className="d-flex flex-column justify-content-center mb-3">
         <h1 className="display-6">Pedidos de la Fecha: </h1>
         {pedidos.map((pedido) => (
           <div key={pedido.id}>
@@ -115,7 +124,8 @@ const PanelGanancias = () => {
           </div>
         ))}
       </div> */}
-    </Container>
+      </Container>
+    </section>
   );
 };
 
