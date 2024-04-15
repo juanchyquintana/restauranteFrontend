53532ebfdbb17d4e2662df40e794575c82630c49
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2024-04-15 04:51:39 -0300

    logo agregado, iconos de registro y links de admin

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,10 +1,10 @@
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import {Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { useState, useEffect } from 'react';
 import './menu.css'
 import { Link, useNavigate, useLocation  } from 'react-router-dom';
 import Button from 'react-bootstrap/Button'
+import brandImg from '../pages/imagenLogo/lotusblanco.png'
 
 const Menu = ({ usuarioLogueado, setUsuarioLogueado }) => {
   const [scrolled, setScrolled] = useState(false);
@@ -57,11 +57,14 @@ const Menu = ({ usuarioLogueado, setUsuarioLogueado }) => {
   }, []);
 
     return (
-      <Navbar expand="lg" variant='dark' bg={`${navbarEstilo()}`} className={`fixed-top navbar-custom text-white bg-body- fs-4 ${scrolled ? 'bg-black shadow' : ''}`}>
+      <Navbar expand="lg" variant='dark' bg={`${navbarEstilo()}`} className={`py-3 fixed-top navbar-custom text-white bg-body- fs-4 ${scrolled ? 'bg-black shadow' : ''}`}>
           <Container fluid className=''>
+            <Navbar.Brand href="#home">{
+              <img src={brandImg} alt="logo Lotus" className='navbarLogo'/>
+            }</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav"/>
             <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center text-center">
-              <Nav className="">
+              <Nav>
                 <Link to="/" className='text-light nav-link'>Inicio</Link>
                 <Link className='text-light nav-link' to="/menu">Menú</Link>
                 <Link to="/contacto" className='text-light nav-link'>Contacto</Link>
@@ -69,33 +72,41 @@ const Menu = ({ usuarioLogueado, setUsuarioLogueado }) => {
                 {
                   usuarioLogueado.tipoUsuario === "admin" ? (
                     <>
-                      <Link to="administrador/cocina"  className='text-light nav-link'>
-                        Cocina
-                      </Link>  
-                      <Link className='text-light nav-link' to="/administrador">
-                        Administrador
-                      </Link>
+                      <NavDropdown title="Administrador" id="navbarScrollingDropdown">
+                      < NavDropdown.Item>
+                          <Link to="/administrador/" className='text-black link link-underline-opacity-0 dropdown-item text-center'>Agregar productos</Link>
+                        </NavDropdown.Item>
+                        <NavDropdown.Item >
+                          <Link to="/administrador/panel-usuarios" className='text-black link-underline-opacity-0 dropdown-item text-center'>Panel Usuario</Link>
+                        </NavDropdown.Item>
+                        <NavDropdown.Item>
+                          <Link to="/administrador/ganancias" className='text-black link link-underline-opacity-0 dropdown-item text-center'>Ganancias</Link>
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href="#action4">
+                          <Link to="/administrador/cocina" className='text-black link link-underline-opacity-0 dropdown-item text-center'>Cocina</Link>
+                        </NavDropdown.Item>
+                      </NavDropdown>
                       <Button className="text-light nav-link" variant="link" onClick={logout}>
-                        Logout
+                        <i className="bi bi-box-arrow-left fs-2"></i>
                       </Button>
                     </>
                   ) : (
                     usuarioLogueado.tipoUsuario === "usuario" ? (
                       <>
-                      <Link className='text-light nav-link' to="/menu">
-                        Menú
+                      <Link className='text-light nav-link' to="/mis-pedidos">
+                        Mis pedidos
                       </Link>  
                       <Link className='text-light nav-link' to="/carrito">
                         Carrito
                       </Link> 
                       <Button className="text-light nav-link" variant="link" onClick={logout}>
-                        Logout
+                      <i className="bi bi-box-arrow-left fs-2"></i>
                       </Button>
                       </>
                     ):(
                     <>
-                      <Link to="/ingresar" className='text-light nav-link'>Login</Link>
-                      <Link to="/registrarse" className='text-light nav-link'>Register</Link>
+                      <Link to="/ingresar" className='text-light nav-link'><i className="bi bi-person fs-1 iconos-nav"></i> Ingresar</Link>
+                      <Link to="/registrarse" className='text-light nav-link'><i class="bi bi-pencil-square fs-2 iconos-nav"></i> Registro</Link>
                     </>)
                   )
                 }
