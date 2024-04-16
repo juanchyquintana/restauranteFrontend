bb2ebd2abd7e1b43df7caab47fd0a2ea822aa8cd
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-04-16 15:11:05 -0300

    se modificó el diseño del modal de los eventos. Se agregó la imagen, el nombre del bar y se agrandó un poco el botón

--- src/components/pages/placas/Cardevento.jsx
+++ src/components/pages/placas/Cardevento.jsx
@@ -10,29 +10,29 @@ import './cardevento.css'
 
 const Cardevento = () => {
   const [show, setShow] = useState(false);
-  const [selectedEvent, setSelectedEvent] = useState(null);
+  const [eventoSeleccionado, setEventoSeleccionado] = useState(null);
 
   const handleClose = () => setShow(false);
-  const handleShow = (event) => {
-    setSelectedEvent(event);
+  const handleShow = (e) => {
+    setEventoSeleccionado(e);
     setShow(true);
   };
 
   const eventos = [
     {
-      title: "Festeja tus cumpleaños",
-      text: "¡Celebra tu cumpleaños con nosotros y convierte tu día en una experiencia única! Ofrecemos una decoración vibrante, servicio excepcional y una deliciosa comida. Únete a nosotros para momentos memorables junto a tus seres queridos. ¡Te esperamos para celebrar de una manera emocionante!",
-      image: Evento,
+      titulo: "Festeja tus cumpleaños",
+      texto: "¡Celebra tu cumpleaños con nosotros y convierte tu día en una experiencia única! Ofrecemos una decoración vibrante, servicio excepcional y una deliciosa comida. Únete a nosotros para momentos memorables junto a tus seres queridos. ¡Te esperamos para celebrar de una manera emocionante!",
+      imagen: Evento,
     },
     {
-      title: "Almuerzos Ejecutivos",
-      text: "¡Ven a disfrutar de nuestro exclusivo almuerzo ejecutivo! Déjate sorprender con un servicio de alta calidad y platos exquisitos. Comparte un ambiente tranquilo con tus compañeros de trabajo. ¡Te esperamos para vivir una experiencia gastronómica única durante tu almuerzo ejecutivo!",
-      image: Evento2,
+      titulo: "Almuerzos Ejecutivos",
+      texto: "¡Ven a disfrutar de nuestro exclusivo almuerzo ejecutivo! Déjate sorprender con un servicio de alta calidad y platos exquisitos. Comparte un ambiente tranquilo con tus compañeros de trabajo. ¡Te esperamos para vivir una experiencia gastronómica única durante tu almuerzo ejecutivo!",
+      imagen: Evento2,
     },
     {
-      title: "Ceremonia del Té",
-      text: "Disfruta de la tranquila y refinada ceremonia del té con nosotros. Sumérgete en la tradición y la elegancia mientras degustas una selección exquisita de tés acompañados de dulces y bocados delicados. Únete a nosotros para una experiencia relajante y enriquecedora que te transportará a un mundo de armonía y serenidad.",
-      image: Evento3,
+      titulo: "Ceremonia del Té",
+      texto: "Disfruta de la tranquila y refinada ceremonia del té con nosotros. Sumérgete en la tradición y la elegancia mientras degustas una selección exquisita de tés acompañados de dulces y bocados delicados. Únete a nosotros para una experiencia relajante y enriquecedora que te transportará a un mundo de armonía y serenidad.",
+      imagen: Evento3,
     },
   ];
 
@@ -44,11 +44,11 @@ const Cardevento = () => {
             <Card key={index} className="col-lg-3 col-md-6 col-sm-6 tamañoCard">
               <Card.Img
                 variant="top"
-                src={evento.image}
+                src={evento.imagen}
                 className="imagenEvento "
               />
               <Card.Body>
-                <Card.Title className="text-center textoEvento">{evento.title}</Card.Title>
+                <Card.Title className="text-center textoEvento">{evento.titulo}</Card.Title>
                 <div className="d-flex justify-content-center">
                   <Button variant="primary" onClick={() => handleShow(evento)} className="textoBoton">
                     ¡Conoce más!
@@ -59,12 +59,14 @@ const Cardevento = () => {
           ))}
         </Row>
       </Container>
-      {selectedEvent && (
+
+      {eventoSeleccionado && (
         <ModalEvento
           show={show}
           handleClose={handleClose}
-          title={selectedEvent.title}
-          body={selectedEvent.text}
+          titulo={eventoSeleccionado.titulo}
+          texto={eventoSeleccionado.texto}
+          imagen={eventoSeleccionado.imagen}
         />
       )}
     </>
