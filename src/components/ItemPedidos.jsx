132e8bd9bcdc2e49763159e54b36fe9b90ba20d5
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-04-17 12:30:56 -0300

    se agregó un switch para chequear en que estado está el pedido y se agrega un color para cada caso

--- src/components/ItemPedidos.jsx
+++ src/components/ItemPedidos.jsx
@@ -5,13 +5,28 @@ const ItemPedidos = ({ pedido }) => {
     return fecha.toLocaleDateString("es-AR", opciones);
   };
 
+  let estadoClass;
+  switch (pedido.estado) {
+    case 'terminado':
+      estadoClass = 'bg-danger text-white';
+      break;
+    case 'proceso':
+      estadoClass = 'bg-success text-white';
+      break;
+    case 'pendiente':
+      estadoClass = 'bg-warning text-white';
+      break;
+    default:
+      estadoClass = '';
+  }
+
   return (
     <tr>
       <td>{pedido.usuario.nombre}</td>
       <td>{formatearFecha(pedido.fecha)}</td>
       <td className="text-uppercase fw-semibold">{pedido.tipoEntrega}</td>
       <td className="text-uppercase fw-semibold">${pedido.total}</td>
-      <td className="text-uppercase fw-bold text-danger text-center">
+      <td className={`text-uppercase fw-bold text-center ${estadoClass}`}>
         {pedido.estado}
       </td>
     </tr>
